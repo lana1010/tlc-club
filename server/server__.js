94e4697724579750874b7975eeeaf28c123420e7
lana1010 <lana1010@gmail.com>
2024-04-05 16:06:01 -0700

    removed unnecessary files

--- server/server__.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const app = require('./index');
-
-const port = process.env.PORT || 3001;
-
-app.listen(port, () => {
-	// console.log(`...listening on port ${port}`);
-})
